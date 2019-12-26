ec540f64cbbf55eacce01b6659ee5427e0c6e3de
djfdmator <djfdmator@gmail.com>
2019-12-26 21:28:14 +0900

    db구조 정리 및 로딩씬 기능 완료
    1.프로그레스바 추가함
    2.로딩씬 연출 해야함
    3. db 편의기능 더 만들어야함

--- ProjectDragon/Assets/Scripts/Map/Loading.cs
+++ ProjectDragon/Assets/Scripts/Map/Loading.cs
@@ -3,9 +3,11 @@
 // Async Loader
 // Load the map asynchronously.
 //
+// 2019-12-26: Add Loading Progress Bar
+//
 //  AUTHOR: Kim Dong Ha
 // CREATED:
-// UPDATED: 2019-12-16
+// UPDATED: 2019-12-26
 // ==============================================================
 
 using System.Collections;
@@ -16,46 +18,108 @@ using UnityEngine.UI;
 
 public class Loading : MonoBehaviour
 {
-    public string nextScene;
-    public UILabel label;
+    public string sceneName = string.Empty;
     AsyncOperation asyncOperation;
-    // Start is called before the first frame update
-    public GameObject next_Button;
+    public UILabel label;
+    public GameObject nextButton;
+
+    //프로그레스 바
+    public UIProgressBar loadingBar;
+    public UILabel loadingLabel;
 
     private void Awake()
     {
-        
-    }
-    void Start()
-    {
-        next_Button = GameObject.Find("NextButton");
-        StartCoroutine(LoadSceneAsync(nextScene));
+        loadingBar = GameObject.Find("ProgressBar").GetComponent<UIProgressBar>();
+        loadingLabel = loadingBar.GetComponent<UILabel>();
+
+        nextButton = GameObject.Find("NextButton");
+        nextButton.GetComponent<BoxCollider>().enabled = false;
     }
 
-    //asyncload
-    IEnumerator LoadSceneAsync(string _sceneName)
+    void Start()
     {
+        StartCoroutine(RegionRepresentation());
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    //지역 넘어가는 연출
+    IEnumerator RegionRepresentation()
+    {
+
         yield return null;
-        int percent;
-        asyncOperation = SceneManager.LoadSceneAsync(_sceneName);
+    }
+
+    void CalculateSceneName()
+    {
+#if UNITY_EDITOR
+        Debug.Log(DataTransaction.Inst.CurrentStage % 4);
+#endif
+        if (DataTransaction.Inst.CurrentStage % 4 == 0)
+        {
+            int region = DataTransaction.Inst.CurrentStage / 4;
+#if UNITY_EDITOR
+            Debug.Log(region);
+#endif
+            switch (region)
+            {
+                case 1:
+                    sceneName = "MaDongSeok";
+                    break;
+                case 2:
+                    sceneName = "MaDongSeok";
+                    break;
+                case 3:
+                    sceneName = "MaDongSeok";
+                    break;
+                case 4:
+                    sceneName = "MaDongSeok";
+                    break;
+            }
+        }
+        else
+        {
+            sceneName = "Map_Generator";
+        }
+    }
+
+    //AsyncLoad
+    IEnumerator LoadSceneAsync()
+    {
+        //CalculateSceneName();
+
+        yield return null;
+
+        float percent = 0.0f;
+        ProgressBar(percent);
+        asyncOperation = SceneManager.LoadSceneAsync(sceneName);
         asyncOperation.allowSceneActivation = false;
 
         while(!asyncOperation.isDone)
         {
+            percent = asyncOperation.progress;
+            ProgressBar(percent);
             yield return null;
-            percent = Mathf.RoundToInt(asyncOperation.progress * 100.0f);
-            label.text = percent.ToString() + "%";
-            if(percent >= 90.0f)
+
+            if (percent >= 0.9f)
             {
+                ProgressBar(percent);
                 yield return new WaitForSeconds(1.0f);
-                label.text = "Next";
-                next_Button.GetComponent<BoxCollider>().enabled = true;
+                //Load Complete
+                ProgressBar(1.0f);
+                nextButton.GetComponent<BoxCollider>().enabled = true;
                 break;
             }
         }
     }
 
-    public void NextButton()
+    void ProgressBar(float _percent)
+    {
+        loadingBar.value = _percent;
+        _percent *= 100.0f;
+        loadingLabel.text = "Loading..." + _percent + "%";
+    }
+
+    public void Button_NextScene()
     {
         asyncOperation.allowSceneActivation = true;
     }
