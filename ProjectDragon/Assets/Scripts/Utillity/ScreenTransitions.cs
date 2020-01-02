1a5ed2b60d45cd27e2633e3181eaa96fc7403085
tpdms025 <timepark25@naver.com>
2020-01-02 20:23:34 +0900

    201002-3
    쉐이더 추가

---
+++ ProjectDragon/Assets/Scripts/Utillity/ScreenTransitions.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class ScreenTransitions : MonoBehaviour
+{
+    public Material TransitionMaterial;
+    public float transitionTime = 1.5f;
+
+    void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        if (TransitionMaterial != null)
+            Graphics.Blit(src, dst, TransitionMaterial);
+    }
+
+    private void Start()
+    {
+        StartCoroutine(EffectStart("Textures/criss_cross_pattern"));
+    }
+
+
+    public IEnumerator EffectStart(string path)
+    {
+        TransitionMaterial.SetTexture("_TransitionTex", Resources.Load<Texture>(path));
+
+        float cutoff = 0f;
+        float time = 0;
+
+        while (time <= transitionTime)
+        {
+            time += Time.deltaTime;
+            cutoff = Mathf.Lerp(0.0f, 1.0f, time / transitionTime);
+            TransitionMaterial.SetFloat("_Cutoff", cutoff);
+
+            yield return null;
+        }
+
+    }
+}
